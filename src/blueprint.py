6184466b34821e39269bad14e969c577b9a02ead
maury <maury.corentin18@gmail.com>
2026-01-14 15:17:22 +0100

    début formulaire

--- src/blueprint.py
+++ src/blueprint.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, render_template
+from flask import Blueprint, abort, jsonify, render_template, request
 from jinja2 import TemplateNotFound
 
 router = Blueprint("simple_page", __name__)
@@ -11,3 +11,9 @@ def show(page):
         return render_template(f"{page}.html")
     except TemplateNotFound:
         abort(404)
+
+@router.route('/submit', methods=['POST'])
+def submit():
+    data = request.form.to_dict(flat=False)  # Permet de récupérer toutes les valeurs cochées
+    print(data)
+    return jsonify(data)
