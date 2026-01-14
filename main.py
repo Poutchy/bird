68c806241f87f1aa440111196cc740012ad52c95
maury <maury.corentin18@gmail.com>
2026-01-14 15:44:31 +0100

    image patte couleur

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from flask import Flask
 
 from src.blueprint import router
 
-app = Flask(__name__, template_folder="src/templates")
+app = Flask(__name__, template_folder="src/templates", static_folder="src/static")
 app.register_blueprint(router)
